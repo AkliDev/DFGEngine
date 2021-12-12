736ce232bd504cbecfb4941081d1850204b9d357
Akli <aklitouati@hotmail.com>
2021-12-12 19:14:02 +0100

    Here we go again.

---
+++ editor/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "We are going to do this editor quick and dirty!" << std::endl;
+	std::cin.get();
+	return 0;
+}
---
+++ engine/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "We are going to do this quick and dirty!" << std::endl;
+	std::cin.get();
+	return 0;
+}
