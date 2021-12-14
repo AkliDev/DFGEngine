764ec21832ae82009ba0680c100a98768fcb889f
Akli <aklitouati@hotmail.com>
2021-12-14 09:14:55 +0100

    Added clad and tidied up cmake structure.

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <SDL.h>
+#include <glad/glad.h>
 
-int main()
+int main(int argc, char* args[])
 {
 	std::cout << "We are going to do this quick and dirty!" << std::endl;
 	std::cin.get();
